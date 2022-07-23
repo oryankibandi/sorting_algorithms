90e9e74aa9752495dfc6d2d5a77d9c0285bda170
oryankibandi <shreckoryan@gmail.com>
2022-07-23 23:40:15 +0300

    fixed betty errors

--- 100-shel_sort.c
+++ 100-shel_sort.c
@@ -7,31 +7,34 @@
  */
 void shell_sort(int *array, size_t size)
 {
-int inner, outer;
-int valueToInsert;
-int interval = 1;
-int i = 0;
+	int inner, outer;
+	int valueToInsert;
+	int interval = 1;
+	int i = 0;
 
-while(interval <= (int) size/3)
-	interval = interval*3 +1;
-
-
-while(interval > 0) {
-
-	for(outer = interval; outer < (int) size; outer++) {
-		valueToInsert = array[outer];
-		inner = outer;
-
-		while(inner > interval -1 && array[inner - interval] >= valueToInsert) {
-		array[inner] = array[inner - interval];
-		inner -=interval;
-		}
-
-		array[inner] = valueToInsert;
+	while (interval <= (int) size / 3)
+	{
+		interval = interval * 3 + 1;
 	}
 
-interval = (interval -1) /3;
-print_array(array, size);
-i++;
-}
+	while (interval > 0)
+	{
+		for (outer = interval; outer < (int) size; outer++)
+		{
+			valueToInsert = array[outer];
+			inner = outer;
+
+			while (inner > interval - 1 && array[inner - interval] >= valueToInsert)
+			{
+				array[inner] = array[inner - interval];
+				inner -= interval;
+			}
+
+			array[inner] = valueToInsert;
+		}
+
+	interval = (interval - 1) / 3;
+	print_array(array, size);
+	i++;
+	}
 }
